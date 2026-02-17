1faae7238c0398128a308e194abf139e4d599380
tsandrewf <tsandrewf@gmail.com>
2026-02-17 15:26:42 +0300

    init

---
+++ src/main/java/ru/jabki/final_todo/FinalTodoApplication.java
@@ -0,0 +1,13 @@
+package ru.jabki.final_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTodoApplication.class, args);
+	}
+
+}
